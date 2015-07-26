3a360c47e41c081fe9ff874a7981eb73427eb072
Munair Simpson <munair@gmail.com>
2015-07-26 10:35:47 +0000

    New full screen video version

--- web.js
+++ web.js
@@ -9,6 +9,7 @@ app.use("/fonts", express.static(__dirname + '/fonts'));
 app.use("/img", express.static(__dirname + '/img'));
 app.use("/js", express.static(__dirname + '/js'));
 app.use("/less", express.static(__dirname + '/less'));
+app.use("/video", express.static(__dirname + '/video'));
 
 app.get('/', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
 
